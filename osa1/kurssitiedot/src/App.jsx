7d02aa4721bd2889ecce4ec44387c9f74e1b86c9
Aaron <a.aspblom@gmail.com>
2024-02-14 20:20:30 +0200

    osa1 part2

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,24 +1,19 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const parts = [
-    { part: part1, exercise: exercises1},
-    { part: part2, exercise: exercises2},
-    { part: part3, exercise: exercises3},
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    { part: 'Fundamentals of React', exercise: 10},
+    { part: 'Using props to pass data', exercise: 7},
+    { part: 'State of a component', exercise: 14}
+    ]
+  }
 
 
   return (
     <div>
-      <Header course={course}/>
-      <Content content={parts}/>
-      <Total exercises={parts}/>
+      <Header course={course.name}/>
+      <Content content={course.parts}/>
+      <Total exercises={course.parts}/>
     </div>
   )
 }
